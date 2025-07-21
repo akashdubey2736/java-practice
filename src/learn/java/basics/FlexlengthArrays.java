dc592eac5fb828856e38a7fb87b1ea804eb61d89
Akash Dubey <akashdubey@Akashs-MacBook-Pro.local>
2025-07-21 16:59:51 -0400

    Update

---
+++ src/learn/java/basics/FlexlengthArrays.java
@@ -0,0 +1,22 @@
+package learn.java.basics;
+
+public class FlexlengthArrays {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World again");
+		
+		String[] splitStrings="Hello world again".split(" ");
+		printText(splitStrings);
+		
+		printText("Hello","World");
+
+	}
+	
+	private static void printText(String... textList) {
+		for(String t : textList) {
+			System.out.println(t);
+		}
+	}
+
+}
